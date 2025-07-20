8db75ffa1545f80a9374d4401aa62579bf905d01
nightpetal <utsavbhandari77@gmail.com>
2025-07-20 08:17:29 +0545

    Commit

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,4 @@ urlpatterns = [
     path("menu-item/", MenuItemList.as_view(), name='menu-item-list'),
     path('menu-orders/', OrderListCreateView.as_view(),name='order-list-create'), 
     path('menu-orders/<int:pk>/', OrderDetailView.as_view(),name='order-detail'),
-    
-    
 ]
